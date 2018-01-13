106f715ac175c2845dec24349910c3209353a8d6
escoand <escoand@users.noreply.github.com>
2018-01-13 15:07:50 +0100

    Update docker-wrapper.sh

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o docker
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o docker
+
 . /appenv/bin/activate
 cd /home/docker
 exec ocrmypdf "$@"
